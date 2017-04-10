4736ef7e9f70806daae203c25f23fed312f4e564
Martin Krauskopf <martin.krauskopf@gmail.com>
2017-04-10 21:53:03 +0200

    Initial game rules and basic animation

--- sketch.js
+++ sketch.js
@@ -12,11 +12,6 @@ function Board(xLines, yLines) {
         });
     });
 
-    // for now, hardcoded initial status
-    this.status[7][5] = 1;
-    this.status[7][6] = 1;
-    this.status[8][6] = 1;
-
 }
 
 
@@ -43,14 +38,66 @@ var sketch = function (p) {
 
         board = new Board(canvasW / lineStep, canvasH / lineStep);
 
-        drawInitialConfiguration();
+        // for now, hardcoded initial status
+        board.status[7][5] = 1;
+        board.status[7][6] = 1;
+        board.status[8][6] = 1;
+        board.status[8][8] = 1;
+        board.status[8][9] = 1;
+        board.status[8][10] = 1;
+
+        drawConfiguration();
+
+        // temporary for easier debugging
+        p.frameRate(3);
     };
 
     p.draw = function () {
-
+        processToNextStep();
+        drawConfiguration();
     };
 
-    var drawInitialConfiguration = function () {
+    var processToNextStep = function () {
+        var nextBoard = new Board(board.xLines, board.yLines);
+        for (var x = 0; x < board.xLines; x++) {
+            for (var y = 0; y < board.yLines; y++) {
+                var nOfNeight = computeNeighbours(x, y);
+                isLive = board.status[x][y] === 1;
+                if (isLive && nOfNeight < 2) {
+                    nextBoard.status[x][y] = 0;
+                } else if (isLive && (nOfNeight === 2 || nOfNeight === 3)) {
+                    nextBoard.status[x][y] = 1;
+                } else if (isLive && nOfNeight > 3) {
+                    nextBoard.status[x][y] = 0;
+                } else if (!isLive && nOfNeight === 3) {
+                    nextBoard.status[x][y] = 1;
+                } else {
+                    nextBoard.status[x][y] = board.status[x][y];
+                }
+            }
+        }
+        board = nextBoard;
+    };
+
+    var computeNeighbours = function (x, y) {
+        return oneForAlive(x, y - 1)
+            + oneForAlive(x + 1, y - 1)
+            + oneForAlive(x + 1, y)
+            + oneForAlive(x + 1, y + 1)
+            + oneForAlive(x, y + 1)
+            + oneForAlive(x - 1, y + 1)
+            + oneForAlive(x - 1, y)
+            + oneForAlive(x - 1, y - 1);
+    };
+
+    var oneForAlive = function (x, y) {
+        if (x < 0 || y < 0 || x >= board.xLines || y >= board.yLines) {
+            return 0;
+        }
+        return board.status[x][y];
+    };
+
+    var drawConfiguration = function () {
         for (var x = 0; x < board.xLines; x++) {
             for (var y = 0; y < board.yLines; y++) {
                 if (board.status[x][y] === 1) {
@@ -58,7 +105,7 @@ var sketch = function (p) {
                 }
             }
         }
-    }
+    };
 
     var drawCell = function (xCell, yCell) {
         var y = yCell * lineStep;
